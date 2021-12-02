41bcf04a8ebed8e778ca47b54f7f8fe850ce55a1
Aun Johnsen <Skippern@users.noreply.github.com>
2021-12-02 22:10:40 +0100

    constvaF.ino
    This was just added to show that constant variables cannot be printed from flash memory - compiler error

---
+++ constvaF.ino
@@ -0,0 +1,53 @@
+// constvar
+//
+// Everything stored in constant variables
+const char filename[] = "constvaF.ino";
+const char demo[] = "A demonstration in code and memory efficiency";
+
+const char loophead[] = "I am a loop!";
+const char iamloop[] = "I am repeated every ";
+const char millis_a[] = " millisecond";
+
+const int looptime = 1000;
+
+const char testtext[] = "This is a test!";
+const int one = 1;
+const int two = 2;
+const int three = 3;
+const float four = 4.0;
+const int five = 5;
+const char add[] = " + ";
+
+void setup() {
+  Serial.println(F(filename));
+  Serial.println();
+  Serial.println(demo);
+  Serial.println();
+}
+
+void loop() {
+  Serial.println(loophead);
+  Serial.print(iamloop);
+  Serial.print(looptime);
+  Serial.println(millis_a);
+  Serial.println();
+  Serial.println();
+  test();
+  delay(looptime);
+}
+
+void test() {
+  Serial.println(testtext);
+
+  Serial.println(four);
+
+  Serial.print(one); Serial.print(add); Serial.println(two);
+  Serial.println(one + two);
+  Serial.print(one); Serial.print(add); Serial.print(two);
+  Serial.print(add); Serial.print(three); Serial.print(add);
+  Serial.print(four); Serial.print(add); Serial.println(five);
+  Serial.println(one + two + three + four + five);
+
+  Serial.println();
+  Serial.println();
+}
